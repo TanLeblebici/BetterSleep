d16b23365b738aea75f4715ffd1d658778f98109
TanLeblebici <75142903+TanLeblebici@users.noreply.github.com>
2022-07-28 21:01:05 +0300

    Add files via upload

---
+++ BetterSleep/BetterSleep/BetterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepApp.swift
+//  BetterSleep
+//
+//  Created by Tan Leblebici on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
